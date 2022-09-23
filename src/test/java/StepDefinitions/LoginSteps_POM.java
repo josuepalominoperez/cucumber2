b7b540ca6e61e4246a94f282da6fc453da27e786
josuepalominoperez <oscar.palomino.perez@gmail.com>
2022-09-22 22:41:30 -0500

    practicando POM

--- src/test/java/StepDefinitions/LoginSteps_POM.java
+++ src/test/java/StepDefinitions/LoginSteps_POM.java
@@ -1,6 +1,5 @@
 package StepDefinitions;
 
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -9,10 +8,12 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import pages.loginPage;
 
-public class LoginSteps {
+public class LoginSteps_POM {
 	
-/*	WebDriver driver = null;
+	WebDriver driver = null;
+	loginPage login;
 	
 	@Given("I open the browser")
 	public void i_open_the_browser() {
@@ -23,28 +24,32 @@ public class LoginSteps {
 
 	@When("I got to DemoQAWebLoginPage")
 	public void i_got_to_demo_qa_web_login_page() {
+		
 		driver.get("https://demoqa.com/login");
 	    
 	}
 
 	@And("^I enter (.*) and (.*)$")
 	public void i_enter_username_and_password(String username , String password) throws InterruptedException {
-	    driver.findElement(By.id("userName")).sendKeys(username);
-	    Thread.sleep(3000);
-	    driver.findElement(By.id("password")).sendKeys(password);
-	    Thread.sleep(3000);
+		login = new loginPage(driver);
+		login.enterUsername(username);
+		Thread.sleep(3000);
+		login.enterPassword(password);
+		Thread.sleep(3000);
+		
 	}
 
 	@And("I clic Login button")
 	public void i_clic_login_button() {
-	    driver.findElement(By.id("login")).click();
+	    login.clickLogin();
 	}
 
 	@Then("I verify the successful login")
 	public void i_verify_the_successful_login() throws InterruptedException {
 		Thread.sleep(3000);
-		driver.findElement(By.id("submit")).click();
+		login.checkLogoutIsDisplayed();
+		driver.quit();
 	}
-*/
+
 
 }
